0147454cf4cd1cd36b80ddde03807020550a6e7f
akash Saroj <akashcs2k21@gmail.com>
2024-01-06 02:19:28 +0530

    saving react

---
+++ 01basicsreact/src/App.jsx
@@ -0,0 +1,15 @@
+import Mode from "./mode"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Mode/>
+      <h1>akash ka react app</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01basicsreact/src/mode.jsx
@@ -0,0 +1,6 @@
+function Mode(){
+    return(
+        <h1>my name is akash</h1>
+    );
+}
+ export default Mode
